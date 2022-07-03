522defe99d3cd9aaaaa7b75d4b2cd6a3936bfd3a
hayden <haydenburger43@gmail.com>
2022-07-02 19:58:54 -0500

    added app.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Weather-Dashboard
+this app was desined to view weather based on the city entered
+uses js html css
+uses open weathermap.orgs api
+uses 'https://source.unsplash.com/1600x900/?nature,water' api
+made by hayden carr
+some code taken from https://www.youtube.com/watch?v=WZNG8UomjSI
---
+++ app.js
@@ -0,0 +1,31 @@
+let weather = {
+    "apiKey": "2b49101ba5f70dc42229c8c03b8861b2",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=2b49101ba5f70dc42229c8c03b8861b2"
+        ).then((response) =>response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        
+        document.querySelector(".city").innerHTML = "Weather in" + name;
+        document.querySelector(".icon").source  = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp;
+        document.querySelector(".humidity").innerText = "humidity: " +humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " +speed + "km/h";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document
+.querySelector(".search button")
+.addEventListener("click", function() {
+    weather.search();
+});
